2633bb7fd84f76c06f79206eaa026b795a23db77
artsemy <artemiy.konoplyanik@gmail.com>
2019-10-02 17:27:35 +0300

    Initial commit

---
+++ src/com/company/Client.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private Socket clientSocket;
+    private BufferedReader reader;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    public void run(){
+        try {
+            try {
+                clientSocket = new Socket("localhost", 4004);
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                System.out.println("Client start");
+                String serverWord;
+                String clientWord = "start";
+                while (!clientWord.equals("exit")) {
+                    serverWord = in.readLine();
+                    System.out.println(serverWord);
+                    clientWord = reader.readLine();
+                    out.write(clientWord + "\n");
+                    out.flush();
+                }
+            } finally {
+                System.out.println("client close");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+}
